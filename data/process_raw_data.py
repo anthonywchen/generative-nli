349060c335c72ba4ad4bc0d82db993babc0946b6
Anthony Chen <anthony.w.chen@outlook.com>
2019-11-27 19:12:35 -0800

    added in processing of data

---
+++ data/process_raw_data.py
@@ -0,0 +1,121 @@
+import csv
+from jsonlines import Reader
+import json
+import os
+from os.path import dirname, isdir, join
+
+def create_dir(directory_name):
+	# Create data directory if it doesn't exist
+	if not isdir(directory_name):
+		os.mkdir(directory_name)
+
+def process_snli_mnli(input_file, output_file):
+	create_dir(dirname(output_file))
+
+	with open(output_file, 'w') as writer:
+		for line in Reader(open(input_file)):
+			label = line['gold_label']
+			if label not in ['entailment', 'neutral', 'contradiction']:
+				continue
+			
+			output_line = {'premise': line['sentence1'],
+						   'hypothesis': line['sentence2'],
+						   'label': label}
+			writer.write(json.dumps(output_line) + '\n')
+
+def process_rte(input_dir, output_file):
+	create_dir(dirname(output_file))
+
+	def process_file(input_file, tag):
+		processed_lines = []
+		for line in Reader(open(input_file)):
+			label = line['label']
+			assert label in ['entailment', 'not_entailment']
+
+			output_line = {'premise': line['premise'],
+						   'hypothesis': line['hypothesis'],
+						   'label': label,
+						   'tag': tag}
+			processed_lines.append(output_line)
+
+		return processed_lines
+
+	processed_lines = process_file(join(input_dir, 'train.jsonl'), tag='train')
+	processed_lines += process_file(join(input_dir, 'val.jsonl'), tag='dev')
+
+	with open(output_file, 'w') as writer:
+		for line in processed_lines:
+			writer.write(json.dumps(line) + '\n')
+
+def process_hans(input_file, output_file):
+	create_dir(dirname(output_file))
+
+	with open(output_file, 'w') as writer:
+
+		with open(input_file, newline='') as f:
+			f.readline() # Skip header
+			for line in csv.reader(f, delimiter='\t'):
+				label = line[0]
+				assert label in ['entailment', 'non-entailment']
+
+				if label == 'non-entailment':
+					label = 'not_entailment'
+
+				output_line = {'premise': line[5],
+			   				   'hypothesis': line[6],
+			   				   'label': label,
+			   				   'tag': line[8]}
+
+				writer.write(json.dumps(output_line) + '\n')
+
+def process_anli(input_dir, output_file):
+	create_dir(dirname(output_file))
+
+	def process_file(input_file):
+		processed_lines = []
+		for line in Reader(open(input_file)):
+			label = line['label']
+			assert label in ['e', 'n', 'c']
+			if label == 'e':
+				label = 'entailment'
+			elif label == 'n':
+				label = 'neutral'
+			else:
+				label = 'contradiction'
+
+			output_line = {'premise': line['context'],
+						   'hypothesis': line['hypothesis'],
+						   'label': label,
+						   'tag': line['tag']}
+			processed_lines.append(output_line)
+
+		return processed_lines
+
+	processed_lines = process_file(join(input_dir, 'R1/test.jsonl'))
+	processed_lines += process_file(join(input_dir, 'R2/test.jsonl'))
+	processed_lines += process_file(join(input_dir, 'R3/test.jsonl'))
+
+	with open(output_file, 'w') as writer:
+		for line in processed_lines:
+			writer.write(json.dumps(line) + '\n')
+
+def main():
+	print('PROCESSING SNLI...')
+	process_snli_mnli('raw_data/snli/snli_1.0_train.jsonl', 'data/snli/train.jsonl')
+	process_snli_mnli('raw_data/snli/snli_1.0_dev.jsonl', 'data/snli/dev.jsonl')
+
+	print('PROCESSING MNLI...')
+	process_snli_mnli('raw_data/mnli/multinli_1.0_train.jsonl', 'data/mnli/train.jsonl')
+	process_snli_mnli('raw_data/mnli/multinli_1.0_dev_matched.jsonl', 'data/mnli/dev.jsonl')
+	
+	print('PROCESSING RTE...')
+	process_rte('raw_data/rte', 'data/rte/test.jsonl')
+
+	print('PROCESSING HANS...')
+	process_hans('raw_data/hans/heuristics_evaluation_set.txt', 'data/hans/test.jsonl')
+
+	print('PROCESSING ANLI...')
+	process_anli('raw_data/anli', 'data/anli/test.jsonl')
+
+if __name__ == '__main__':
+	main()
--- setup.sh
+++ setup.sh
@@ -3,23 +3,23 @@
 #########################################
 ### Sets up a virtualenv and installs Python packages
 ##########################################
-virtualenv -p python3 generative-nli-env
-source generative-nli-env/bin/activate
+# virtualenv -p python3 generative-nli-env
+# source generative-nli-env/bin/activate
 
-# Install the CUDA10.0 version of torch-1.2 since we have CUDA10.0 installed
-pip install https://download.pytorch.org/whl/cu100/torch-1.2.0-cp36-cp36m-manylinux1_x86_64.whl
-pip install https://download.pytorch.org/whl/cu100/torchvision-0.4.0-cp36-cp36m-manylinux1_x86_64.whl
+# # Install the CUDA10.0 version of torch-1.2 since we have CUDA10.0 installed
+# pip install https://download.pytorch.org/whl/cu100/torch-1.2.0-cp36-cp36m-manylinux1_x86_64.whl
+# pip install https://download.pytorch.org/whl/cu100/torchvision-0.4.0-cp36-cp36m-manylinux1_x86_64.whl
 
-# Install the all pip packages
-pip install -r requirements.txt
+# # Install the all pip packages
+# pip install -r requirements.txt
 
-# Install APEX
-git clone https://github.com/NVIDIA/apex
-cd apex
-pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
-cd ../
+# # Install APEX
+# git clone https://github.com/NVIDIA/apex
+# cd apex
+# pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+# cd ../
 
-##########################################
+# ##########################################
 ### Downloads data into raw data directory
 ##########################################
 mkdir -p raw_data
@@ -28,19 +28,37 @@ cd raw_data/
 #### DATASETS FOR TRAINING
 
 # Download SNLI
+wget https://nlp.stanford.edu/projects/snli/snli_1.0.zip
+unzip snli_1.0.zip
+rm -r __MACOSX snli_1.0.zip
+mv snli_1.0 snli
 
 # Download MNLI
+wget https://www.nyu.edu/projects/bowman/multinli/multinli_1.0.zip
+unzip multinli_1.0.zip
+rm -r __MACOSX multinli_1.0.zip
+mv multinli_1.0 mnli
 
 #### DATASETS FOR EVALUATING GENERALIZATION
 
-# Download QNLI
-
 # Download RTE
+wget https://dl.fbaipublicfiles.com/glue/superglue/data/v2/RTE.zip
+unzip RTE.zip
+rm RTE.zip
+mv RTE rte
 
 # Download SCITAIL
 
 #### ADVERSARIAL DATASETS FOR EVALUATING GENERALIZATION
 
 # Download Adversarial NLI
+wget https://dl.fbaipublicfiles.com/anli/anli_v0.1.zip
+unzip anli_v0.1.zip
+rm -r __MACOSX anli_v0.1.zip
+mv anli_v0.1 anli
 
 # Download HANS
+mkdir hans
+cd hans
+wget https://raw.githubusercontent.com/tommccoy1/hans/master/heuristics_evaluation_set.txt
+cd ../
