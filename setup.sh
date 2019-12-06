62bf423343b66c1e7719b8b11be5e8c7dab03269
Anthony Chen <anthony.w.chen@outlook.com>
2019-12-06 10:50:49 -0800

    mnli bert 32 bs 1e-5lr

--- setup.sh
+++ setup.sh
@@ -3,21 +3,21 @@
 #########################################
 ### Sets up a virtualenv and installs Python packages
 ##########################################
-# virtualenv -p python3 generative-nli-env
-# source generative-nli-env/bin/activate
+virtualenv -p python3 generative-nli-env
+source generative-nli-env/bin/activate
 
-# # Install the CUDA10.0 version of torch-1.2 since we have CUDA10.0 installed
-# pip install https://download.pytorch.org/whl/cu100/torch-1.2.0-cp36-cp36m-manylinux1_x86_64.whl
-# pip install https://download.pytorch.org/whl/cu100/torchvision-0.4.0-cp36-cp36m-manylinux1_x86_64.whl
+# Install the CUDA10.0 version of torch-1.2 since we have CUDA10.0 installed
+pip install https://download.pytorch.org/whl/cu100/torch-1.2.0-cp36-cp36m-manylinux1_x86_64.whl
+pip install https://download.pytorch.org/whl/cu100/torchvision-0.4.0-cp36-cp36m-manylinux1_x86_64.whl
 
-# # Install the all pip packages
-# pip install -r requirements.txt
+# Install the all pip packages
+pip install -r requirements.txt
 
-# # Install APEX
-# git clone https://github.com/NVIDIA/apex
-# cd apex
-# pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
-# cd ../
+# Install APEX
+git clone https://github.com/NVIDIA/apex
+cd apex
+pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+cd ../
 
 # ##########################################
 ### Downloads data into raw data directory
---
+++ test_bert_generalization.py
@@ -0,0 +1,9 @@
+import argparse
+from json import load, loads, dumps
+from os.path import isdir, join
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
