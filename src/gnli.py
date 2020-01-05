added in calculation of hypothesis logits and of class probabilities to gnli model. updated dataset reader of gnli so that lengths are in the correct shape

src/gnli.py
@@ -1,5 +1,6 @@
 """ Natural langauge inference model wrapper for BERT and ROBERTA """
 import logging
+import math
 import numpy
 from overrides import overrides
 from pytorch_transformers import BertModel, RobertaModel
@@ -48,10 +49,6 @@ class GNLI(Model):
 		self._bart = torch.hub.load('pytorch/fairseq', pretrained_model).model
 		self._extend_embeddings()
 
-		# Set linear layer that projects from the embedding size to the vocab size
-		embedding_dim, vocab_size = self._bart.encoder.embed_tokens.weight.size()
-		self._linear_layer = FeedForward(input_dim=embedding_dim, hidden_dims=vocab_size, num_layers=1, activations=['linear'])
-		
 		self.discriminative_loss_weight = discriminative_loss_weight
 		self.metrics = {'accuracy': CategoricalAccuracy()}
 		self.ce_loss = torch.nn.CrossEntropyLoss()
@@ -60,50 +57,6 @@ class GNLI(Model):
 		# Log the number of trainable parameters in the model
 		number_params = sum([numpy.prod(p.size()) for p in list(self.parameters()) if p.requires_grad])
 		logger.info('Number of trainable model parameters: %d', number_params)
-
-	@overrides
-	def forward(self, 
-				src: torch.Tensor, 					# src.size()	 			= [batch_size, 3, premise_len]
-				src_lengths: torch.Tensor, 			# src_lengths.size() 		= [batch_size, 3]
-				prev_output_tokens: torch.Tensor, 	# prev_output_tokens.size() = [batch_size, 3, hypothesis_len]
-				target: torch.Tensor,				# target.size() 			= [batch_size, 3, hypothesis_len]	
-				target_lengths: torch.Tensor, 		# target_lengths.size()		= [batch_size, 3]
-				label: torch.Tensor = None,	 		# label.size() 				= [batch_size]
-				metadata = None):
-		premise_len = src.size(-1)
-		batch_size, num_classes, hypothesis_len = target.size(-1)
-		assert num_classes == 3
-
-		# Resize the inputs so the tensors are two-dimensional and feed through BART
-		src = src.resize(batch_size*num_classes, premise_len)
-		src_lengths = src.resize(batch_size*num_classes)
-		prev_output_tokens = prev_output_tokens.resize(batch_size*num_classes, hypothesis_len)
-		outputs, extra = self._bart(src_tokens=src, src_lengths=src_lengths, prev_output_tokens=prev_output_tokens)
-
-		# # logits.size() = [batch_size, 3, hypothesis_len]
-		# logits = self._linear_layer(cls_embed).float()
-		
-		# output_dict = {'class_probabilities': torch.nn.functional.softmax(logits, dim=-1),
-		# 			   'predicted_label': torch.max(logits, dim=-1)[1],
-		# 			   'metadata': metadata}
-
-		# if label is not None:		
-		# 	label = label.long()
-		# 	output_dict['label'] = label
-
-		# 	# Discriminative Loss
-
-		# 	# Generative Loss
-
-		# 	# Mix the two losses
-		# 	self.metrics['accuracy'](logits, label)
-		# 	output_dict['loss'] = self.ce_loss(logits, label)
-
-		# return output_dict
-
-	@overrides
-	def get_metrics(self, reset: bool = False) -> Dict[str, float]:
-		return {metric_name: metric.get_metric(reset) for metric_name, metric in self.metrics.items()}
 
 	def _extend_embeddings(self):
 		old_embeddings = self._bart.encoder.embed_tokens
@@ -119,3 +72,121 @@ class GNLI(Model):
 		self._bart.encoder.embed_tokens = new_embeddings
 		self._bart.decoder.embed_tokens = self._bart.encoder.embed_tokens
 		assert self._bart.decoder.embed_tokens == self._bart.encoder.embed_tokens
+
+	@overrides
+	def forward(self, 
+				src: torch.Tensor, 					# src.size()	 			= [batch_size, 3, premise_length]
+				src_lengths: torch.Tensor, 			# src_lengths.size() 		= [batch_size, 3]
+				prev_output_tokens: torch.Tensor, 	# prev_output_tokens.size() = [batch_size, 3, hypothesis_length]
+				target: torch.Tensor,				# target.size() 			= [batch_size, 3, hypothesis_length]	
+				target_lengths: torch.Tensor, 		# target_lengths.size()		= [batch_size, 3]
+				label: torch.Tensor = None,	 		# label.size() 				= [batch_size]
+				metadata = None):
+		premise_length = src.size(-1)
+		batch_size, num_classes, hypothesis_length = target.size()
+		assert num_classes == 3
+
+		## Resize the input tensors so that the first and second dimension are merged
+		src = src.resize(batch_size*num_classes, premise_length)
+		src_lengths = src_lengths.resize(batch_size*num_classes)
+		prev_output_tokens = prev_output_tokens.resize(batch_size*num_classes, hypothesis_length)
+		
+		## Feed through BART model, which returns the logits over `prev_output_tokens`
+		#  target_token_logits.size() = [batch_size*3, hypothesis_length, vocab_size]
+		all_token_logits, _ = self._bart(src_tokens=src, src_lengths=src_lengths, prev_output_tokens=prev_output_tokens)
+		all_token_logits = all_token_logits.resize(batch_size, num_classes, hypothesis_length, self._bart.encoder.embed_tokens.num_embeddings)
+		
+		## Calculate hypothesis token probabilities.
+		#  This is useful for visualizing what token most strongly influences the classification decision.
+		all_token_probabilties = torch.nn.functional.softmax(all_token_logits, dim=-1)
+		# hypothesis_probabilities.size() = [batch_size, num_classes, hypothesis_length]
+		# Add a small constant to prevent 0 probabilties since that would set the prob of the hypothesis to 0.
+		hypothesis_probabilities = 1e-15 + torch.gather(all_token_probabilties, dim=-1, index=target.unsqueeze(-1)).squeeze(-1)
+
+		## Calculate the logits for the classes
+		#  class_logits.size() = [batch_size, 3]
+		class_logits = self.calculate_class_logits(hypothesis_probabilities, target_lengths)
+
+		output_dict = {'hypothesis_probabilities': hypothesis_probabilities,
+					   'class_probabilities': torch.nn.functional.softmax(class_logits, dim=-1),
+					   'predicted_label': torch.max(class_logits, dim=-1)[1],
+					   'target_lengths': target_lengths,
+					   'metadata': metadata}
+
+		if label is not None:		
+			label = label.long()
+			output_dict['label'] = label
+			self.metrics['accuracy'](class_logits, label)
+			
+			# Discriminative Loss
+			discriminative_loss = 0
+
+			# Generative Loss
+			generative_loss = 0
+
+			# Mix the two losses
+			output_dict['loss'] = self.discriminative_loss_weight*discriminative_loss + (1-self.discriminative_loss_weight)*generative_loss
+
+		return output_dict
+
+	def calculate_class_logits(self, hypothesis_probabilities, target_lengths):
+		""" Calculates the class logits from the probabilties of the hypothesis tokens.
+		
+		Essentially, this calculates p(hypothesis | premise, label) for the three classes
+		by calculating the autoregressive probability over the hypothesis tokens as the logits for classes.
+		
+		HOWEVER, we cannot calculate p(hypothesis | .) directly, since continually multiplying
+		probabilties will quickly results in underflow.
+		
+		WE SOLVE THIS PROBLEM IN A HACKY WAY. 
+
+		We rely on the fact that when calculating the softmax, multiplying
+		the inputs to the softmax by a constant will preserve the resulting probabilties. 
+
+		We set this constant as ten to the negative base10 value of the largest 
+		probabiltiy would be for the classes if we did directly compute p(h | .) 
+		(`10**min_base10_factor`)
+		
+		HOWEVER, we cannot use this constant directly, since it itself may result in an overflow if 
+		`min_base10_factor` is very large.
+		
+		Thus, we iterate through each token and multiply its probability 
+		by 10 to the negative base10 of the probability while that amount we multiply by has not exceeded 
+		`10**min_base10_factor. This ensures that at the end we have multiplied by our constant while
+		preventing overflow.
+		
+		The resulting logits preseve the probabilites when passed through a softmax function while 
+		preventing underflow.
+		"""
+		batch_size, num_classes, _ = hypothesis_probabilities.size()
+		
+		# Initialize logits as all 1's. We will multiply by the target token probs.
+		class_logits = torch.ones(batch_size, num_classes)
+		
+		# Each data point has a different # of hypothesis tokens so handle so data point iteratively
+		for batch_entry in range(batch_size):
+			target_len = target_lengths[batch_entry]
+			
+			# This is the smallest (negative) base 10 of p(h| premise, label) for this batch entry across all labels.
+			min_base10_factor = torch.min(torch.sum(torch.ceil(-1*torch.log10(hypothesis_probabilities[batch_entry, :, :target_len])), dim=-1)).item()
+			
+			for label_entry in range(num_classes):
+				multiplicative_factor = min_base10_factor
+				
+				for hypothesis_entry in range(target_len):
+					cur_token_prob = hypothesis_probabilities[batch_entry, label_entry, hypothesis_entry]
+					
+					cur_base10_factor = math.ceil(math.log10(cur_token_prob.item())*-1)
+					cur_base10_factor = min(cur_base10_factor, multiplicative_factor)
+					
+					class_logits[batch_entry, label_entry] *= cur_token_prob*(10**cur_base10_factor)
+
+					multiplicative_factor -= cur_base10_factor
+				
+				# Check that we have multiplied the label logit by `10**min_base10_factor`
+				assert multiplicative_factor == 0
+		return class_logits
+
+	@overrides
+	def get_metrics(self, reset: bool = False) -> Dict[str, float]:
+		return {metric_name: metric.get_metric(reset) for metric_name, metric in self.metrics.items()}

src/gnli_dataset_reader.py
@@ -88,11 +88,10 @@ class GNLIDatasetReader(DatasetReader):
 			target += decoder_padding
 			prev_output_tokens += decoder_padding
 
-		# Replicate the hypothesis and the lengths three times
+		# Replicate the hypothesis and the `src_lengths` three times
 		target = [target]*3
 		prev_output_tokens = [prev_output_tokens]*3
 		src_length = [src_length]*3
-		target_length = [target_length]*3
 
 		####################
 		##### Create instance
@@ -104,10 +103,10 @@ class GNLIDatasetReader(DatasetReader):
 					'label': label, 'tag': tag}
 
 		fields = {'src':	 			ArrayField(np.array(src), dtype=np.int64),
-				  'src_lengths': 		ArrayField(np.array([src_length]), dtype=np.int64),
+				  'src_lengths': 		ArrayField(np.array(src_length), dtype=np.int64),
 				  'prev_output_tokens': ArrayField(np.array(prev_output_tokens), dtype=np.int64),
 				  'target': 			ArrayField(np.array(target), dtype=np.int64),
-				  'target_lengths':		ArrayField(np.array([target_length]), dtype=np.int64),
+				  'target_lengths':		ArrayField(np.array(target_length), dtype=np.int64),
 				  'metadata': 			MetadataField(metadata)}
 
 		if label is not None: