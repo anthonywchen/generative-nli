ef83c9606c04387e65d3b65cbe67336d9bb230ec
Anthony Chen <anthonc9@ava-s0.ics.uci.edu>
2020-02-10 09:22:16 -0800

    remove files in out/ directory

--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,5 @@ from src.bert import BertNLI
 from src.bert_dataset_reader import BertNLIDatasetReader
 from src.gnli import GNLI
 from src.gnli_dataset_reader import GNLIDatasetReader
-from src.gnli_embedding import GNLIEmbedding
-# from src.gnli_tokenizer import GNLITokenizer
+# from src.gnli_embedding import GNLIEmbedding
+from src.gnli_tokenizer import GNLITokenizer
